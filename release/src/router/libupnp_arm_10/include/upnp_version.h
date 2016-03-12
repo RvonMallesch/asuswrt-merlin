d95b8f66c3f6a6ec33873bbd3d6dedb590dcef0e
root <root@ubuntu-test-rvm.nexus.csiro.au>
2016-03-12 22:14:38 +1100

    added untracked files

---
+++ release/src/router/libupnp_arm_10/include/upnp_version.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2015, Broadcom Corporation
+ * All Rights Reserved.
+ * 
+ * This is UNPUBLISHED PROPRIETARY SOURCE CODE of Broadcom Corporation;
+ * the contents of this file may not be disclosed to third parties, copied
+ * or duplicated in any form, in whole or in part, without the prior
+ * written permission of Broadcom Corporation.
+ *
+ *
+ * <<Broadcom-WL-IPTag/Proprietary:>>
+ *
+ * $Id: version.h.in,v 1.1 2010-02-11 04:08:13 $
+ *
+*/
+
+#ifndef __LIBUPNP_VERSION_H__
+#define __LIBUPNP_VERSION_H__
+
+#define	UPNP_MAJOR_VERSION	10
+
+#define	UPNP_MINOR_VERSION	10
+
+#define	UPNP_RC_NUMBER		22
+
+#define	UPNP_INCREMENTAL_NUMBER	0
+
+#define	UPNP_BUILD_NUMBER	0
+
+#define	UPNP_VERSION		10, 10, 22, 0
+
+#define	UPNP_VERSION_NUM	0x0a0a1600
+
+/* Driver Version String, ASCII, 32 chars max */
+#define	UPNP_VERSION_STR	"10.10.22.0"
+
+#endif /* __LIBUPNP_VERSION_H__ */
